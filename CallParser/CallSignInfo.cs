994dfe38d56dc4b7f6cc7ad7bb5dd1fb1b878cf6
Peter Bourget <pbourget@w6op.com>
2020-04-08 14:00:18 -0700

    Removed private backing fields as they were not needed.

--- CallParser/CallSignInfo.cs
+++ CallParser/CallSignInfo.cs
@@ -9,8 +9,7 @@ namespace W6OP.CallParser
     {
         public CallSignInfo(System.Xml.Linq.XElement element)
         {
-            //prefixKey = new Dictionary<string, byte>();
-            BuildCallSignInfo(element);
+            InitializeCallSignInfo(element);
         }
 
         public CallSignInfo()
@@ -23,88 +22,44 @@ namespace W6OP.CallParser
         }
 
         /// <summary>
-        /// private fields
-        /// </summary>
-        private Dictionary<string, byte> prefixKey; // key for making this object hashable for searches
-        private int dxcc;  //dxcc_entity
-        private int wae;
-        private string iota;
-        private string wap;
-        private string cq;           //cq_zone
-        private string itu;          //itu_zone
-        private string admin1;
-        private string latitude;     //lat
-        private string longitude;    //long
-        private List<CallSignFlags> callSignFlags = new List<CallSignFlags>();
-
-        private string continent;     //continent
-        private string timeZone;     //time_zone
-        private string admin2;
-        private string name;
-        private string qth;
-        private string comment;
-        private long callbookEntry;
-        //public string CallbookEntry: Pointer; //todo: find out data sources
-
-        private PrefixKind kind;     //kind
-        private string baseCall;
-        private string fullPrefix;
-        private string mainPrefix;
-        private string hitPrefix;   // the search term that got the hit - used for debugging
-        private string country;       //country
-        private string province;     //province 
-
-        private string startDate;
-        private string endDate;
-        private bool isIota = false;
-
-        private string fullCallSign; // I put the call sign here only for pfDXCC types for reference/debugging
-
-        /// <summary>
         /// public properties
         /// </summary>
-        //public Dictionary<string, byte> PrefixKey { get => prefixKey; set => prefixKey = value; }
-        public int DXCC => dxcc;
-        public int WAE { get => wae; }
-        public string Iota { get => iota; set => iota = value; }
-        public string WAP { get => wap; }
-        public string Cq { get => cq; }
-        public string Itu { get => itu; }
-        public string Admin1 { get => admin1; }
-        public string Latitude { get => latitude; }
-        public string Longitude { get => longitude; }
-        
+        public int DXCC { get; set; }
+        public int WAE { get; set; }
+        public string Iota { get; set; }
+        public string WAP { get; set; }
+        public string CQ { get; set; }
+        public string ITU { get; set; }
+        public string Admin1 { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
 
         //from callbook -----------------------------
-        public string Continent { get => continent;  set => continent = value; }
-        public string TimeZone { get => timeZone; set => timeZone = value; }
-        public string Admin2 { get => admin2;  set => admin2 = value; }
-        public string Name { get => name; set => name = value; }
-        public string QTH { get => qth;  set => qth = value; }
-        public string Comment { get => comment;  set => comment = value; }
-        public long CallbookEntry { get => callbookEntry; set => callbookEntry = value; }
+        public string Continent { get; set; }
+        public string TimeZone { get; set; }
+        public string Admin2 { get; set; }
+        public string Name { get; set; }
+        public string QTH { get; set; }
+        public string Comment { get; set; }
+        public long CallbookEntry { get; set; }
         // ----------------------------------------
-        public PrefixKind Kind { get => kind; }
-        public string Country { get => country; }
-        public string Province { get => province; }
-        public string StartDate { get => startDate; }
-        public string EndDate { get => endDate; }
-        public bool IsIota { get => isIota; set => isIota = value; }
-        public string CallSign { get => fullCallSign; set => fullCallSign = value; }
-        public string BaseCall { get => baseCall; set => baseCall = value; }
-        public string FullPrefix { get => fullPrefix; set => fullPrefix = value; }
-        public string MainPrefix { get => mainPrefix; set => mainPrefix = value; }
-        public string HitPrefix { get => hitPrefix; set => hitPrefix = value; }
-        public List<CallSignFlags> CallSignFlags { get => callSignFlags; set => callSignFlags = value; }
+        public PrefixKind Kind { get; set; }
+        public string Country { get; set; }
+        public string Province { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public bool IsIota { get; set; }
+        public string CallSign { get; set; }
+        public string BaseCall { get; set; }
+        public string FullPrefix { get; set; }
+        public string MainPrefix { get; set; }
+        public string HitPrefix { get; set; }
+        public List<CallSignFlags> CallSignFlags { get; set; }
 
-        private void BuildCallSignInfo(XElement prefixXml)
+        private void InitializeCallSignInfo(XElement prefixXml)
         {
             string currentValue;
            
-
-            var points = prefixXml.Descendants("country");
-            var point = points.FirstOrDefault();
-
             foreach (XElement element in prefixXml.Elements())
             {
                 currentValue = element.Value;
@@ -114,70 +69,70 @@ namespace W6OP.CallParser
                     case "masks":
                     break;
                     case "label":
-                        fullPrefix = currentValue ?? "";
-                        if (fullPrefix.Contains("."))
+                        FullPrefix = currentValue ?? "";
+                        if (FullPrefix.Contains("."))
                         {
                             // get string after the "."
-                            mainPrefix = fullPrefix.Substring(fullPrefix.LastIndexOf('.') + 1);
+                            MainPrefix = FullPrefix.Substring(FullPrefix.LastIndexOf('.') + 1);
                         }
                         else
                         {
-                            mainPrefix = fullPrefix;
+                            MainPrefix = FullPrefix;
                         }
                         break;
                     case "kind":
-                        kind = EnumEx.GetValueFromDescription<PrefixKind>(currentValue);
+                        Kind = EnumEx.GetValueFromDescription<PrefixKind>(currentValue);
                         //initialize since DXCC.Kind does not have province field
                         //-prevent user having to check for null on a common field
-                        if (kind == PrefixKind.DXCC)
+                        if (Kind == PrefixKind.DXCC)
                         {
-                            province = "";
+                            Province = "";
                         }
                         break;
                     case "country":
-                        country = currentValue ?? "";
+                        Country = currentValue ?? "";
                         break;
                     case "province":
-                        province = currentValue ?? "";
+                        Province = currentValue ?? "";
                         break;
                     case "dxcc_entity":
-                        dxcc = Convert.ToInt32(currentValue);
+                        DXCC = Convert.ToInt32(currentValue);
                         break;
                     case "cq_zone":
-                        cq = currentValue ?? "";
+                        CQ = currentValue ?? "";
                         break;
                     case "itu_zone":
-                        itu = currentValue ?? "";
+                        ITU = currentValue ?? "";
                         break;
                     case "continent":
-                        continent = currentValue ?? "";
+                        Continent = currentValue ?? "";
                         break;
                     case "time_zone":
-                        timeZone = currentValue ?? "";
+                        TimeZone = currentValue ?? "";
                         break;
                     case "lat":
-                        latitude = currentValue ?? "";
+                        Latitude = currentValue ?? "";
                         break;
                     case "long":
-                        longitude = currentValue ?? "";
+                        Longitude = currentValue ?? "";
                         break;
                     case "city":
-                        qth = currentValue ?? "";
+                        QTH = currentValue ?? "";
                         break;
                     case "wap_entity":
-                        wap = currentValue ?? "";
+                        WAP = currentValue ?? "";
                         break;
                     case "wae_entity":
-                        wae = Convert.ToInt32(currentValue);
+                        WAE = Convert.ToInt32(currentValue);
                         break;
                     case "province_id":
-                        admin1 = currentValue ?? "";
+                        Admin1 = currentValue ?? "";
                         break;
                     case "start_date":
-                        startDate = currentValue ?? "";
+                        StartDate = currentValue ?? "";
                         break;
                     case "end_date":
-                        endDate = currentValue ?? "";
+                        EndDate = currentValue ?? "";
                         break;
                     default:
                         currentValue = null;
